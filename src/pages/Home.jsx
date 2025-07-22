36019d2eb2d25fda9a1928d8b838f99a0440600e
jamalt23 <jamalt.live@gmail.com>
2025-07-22 20:01:41 +0400

    Refactor Home component structure and enhance button styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router';
+
 const Home = () => {
   return (
-    <img src="images/hero.jpg" alt="Hero" />
-  )
-}
+    <>
+      <div className="relative h-[90vh]">
+        <img className="h-full w-full object-cover" src="images/hero.jpg" alt="Hero" />
+        <div className="absolute bottom-0 flex w-full flex-col items-start justify-between gap-y-4 p-8 font-bold lg:flex-row">
+          <h2 className="text-4xl text-white uppercase">For any transition</h2>
+          <Link className="btn bg-white text-[15px] leading-[17.25px] hover:underline">Shop the news</Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;

cf23493e90079a705ad4ab83ce0f243d2146bd1e
jamalt23 <jamalt.live@gmail.com>
2025-07-27 23:47:14 +0400

    Refactor Loading component to accept backdrop prop for conditional styling

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,8 @@
 import clsx from 'clsx';
 
-const Loading = () => {
+const Loading = ({ backdrop = false }) => {
   return (
-    <div className="fixed z-100 h-screen w-screen bg-black/25">
+    <div className={clsx('fixed z-100 h-screen w-screen', backdrop && 'bg-black/25')}>
       <div
         className={clsx(
           'absolute top-1/2 left-1/2 size-14 -translate-1/2 animate-spin',

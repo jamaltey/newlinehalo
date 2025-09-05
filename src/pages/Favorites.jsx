0f81cf6be4995d2863e0ec516283d526eb9f024c
jamalt23 <jamal.tey@icloud.com>
2025-09-05 14:22:51 +0400

    Update Profile component: enhance user validation logic for navigation

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -2,7 +2,7 @@ const Favorites = () => {
   return (
     <div>
       <h1 className="mb-2 text-center text-2xl font-medium uppercase">My favorites</h1>
-      <p className="mb-8 text-center font-light text-sm">
+      <p className="mb-8 text-center text-sm font-light">
         My Favourites is a great way to keep track of your personal must-haves and their
         availability. And, most importantly, it just takes one click to move an item from the list
         and into your shopping cart.
